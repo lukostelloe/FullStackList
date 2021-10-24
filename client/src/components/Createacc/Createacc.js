227984ecef9f3f8809a62cafd3db350c1731dca2
lukostelloe <lukostelloe@gmail.com>
2021-10-24 17:50:30 +0200

    password functionality

--- client/src/components/Createacc/Createacc.js
+++ client/src/components/Createacc/Createacc.js
@@ -1,19 +1,51 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Createacc.css";
 import { Link } from "react-router-dom";
 
 function Createacc() {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const [cpassword, setcpassword] = useState("");
+
+  const handleSubmit = (e) => {
+    setusername(e.target.value);
+    setpassword(e.target.value);
+    setcpassword(e.target.value);
+    console.log({ username }, { password }, { cpassword });
+
+    if (password !== cpassword) {
+      console.log("passwords don't match!");
+    }
+  };
+
   return (
     <div className="login_div">
       <h2>Create Account</h2>
       <form className="login_form">
         <label htmlFor="username"> Create Username</label>
-        <input type="text" placeholder="username"></input>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setusername(e.target.value)}
+        ></input>
         <label htmlFor="password">Create Password</label>
-        <input type="text" placeholder="password"></input>
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setpassword(e.target.value)}
+        ></input>
         <label htmlFor="confirm-password">Confirm Password</label>
-        <input type="text" placeholder="confirm password"></input>
-        <button type="submit">Create Account</button>
+        <input
+          type="text"
+          placeholder="confirm password"
+          value={cpassword}
+          onChange={(e) => setcpassword(e.target.value)}
+        ></input>
+        <button type="button" onClick={handleSubmit}>
+          Create Account
+        </button>
         <Link to="/login">
           <li>Already have an account?</li>
         </Link>
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,15 +1,36 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+
+  const handleSubmit = (e) => {
+    setusername(e.target.value);
+    setpassword(e.target.value);
+    console.log({ username }, { password });
+  };
+
   return (
     <div className="login_div">
       <h2>Login</h2>
       <form className="login_form">
-        <input type="text" placeholder="username"></input>
-        <input type="text" placeholder="password"></input>
-        <button type="submit">Log In</button>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setusername(e.target.value)}
+        ></input>
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setpassword(e.target.value)}
+        ></input>
+        <button type="button" onClick={handleSubmit}>
+          Log In
+        </button>
         <Link to="/createacc">
           <li>Don't have an account?</li>
         </Link>
