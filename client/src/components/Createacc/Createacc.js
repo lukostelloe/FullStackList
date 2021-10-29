5b8c87d4ce5475c30ec56ae9983fc44e076ed6a7
lukostelloe <lukostelloe@gmail.com>
2021-10-29 11:37:17 +0200

    regex working

--- client/src/components/Createacc/Createacc.js
+++ client/src/components/Createacc/Createacc.js
@@ -8,39 +8,36 @@ function Createacc() {
   const [password, setpassword] = useState("");
   const [confpass, setconfpass] = useState("");
   const [userlist, setuserlist] = useState([]);
-  const [passerr, setpasserr] = useState(false);
-
-  // const validPass = new RegExp("^[a-zA-Z0-9._:$!%-]$");
+  const [warning, setwarning] = useState(false);
 
   const validPass = new RegExp(/^(?=.*?[a-z][A-Z][0-9]).+$/);
 
-  const addToList = () => {
+  const addToList = async () => {
     if (!validPass.test(password)) {
       console.log("password not right");
+      setwarning(true);
     } else {
       console.log("cool password bro");
+      setwarning(false);
+      if (password === confpass) {
+        try {
+          const response = await Axios.post(
+            "http://localhost:3001/insertlogin",
+            {
+              username: username,
+              password: password,
+            }
+          );
+          setuserlist([...userlist, response.data]);
+          console.log("account created!");
+        } catch (error) {
+          console.log("there is an error with addToList function");
+        }
+      } else {
+        console.log("pass and conf don't match");
+      }
     }
   };
-
-  // const addToList = async () => {
-  //   if (password.length < 5) {
-  //     console.log("password too small");
-  //   }
-  //   if (password === confpass && password.length > 4) {
-  //     try {
-  //       const response = await Axios.post("http://localhost:3001/insertlogin", {
-  //         username: username,
-  //         password: password,
-  //       });
-  //       setuserlist([...userlist, response.data]);
-  //       console.log("account created!");
-  //     } catch (error) {
-  //       console.log("there is an error with addToList function");
-  //     }
-  //   } else {
-  //     console.log("pass and conf don't match");
-  //   }
-  // };
 
   return (
     <div className="login_div">
@@ -65,7 +62,9 @@ function Createacc() {
             value={confpass}
             onChange={(e) => setconfpass(e.target.value)}
           ></input>
-          <p>password must be at least 5 characters</p>
+          <p className={warning ? "nopass" : "pass"}>
+            password must contain upper, lower and number
+          </p>
           <button type="button" onClick={addToList}>
             Create Account
           </button>
