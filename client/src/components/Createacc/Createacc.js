17bd4e9d6f3d877e6220701786c58fedf6ad02c5
lukostelloe <lukostelloe@gmail.com>
2021-10-29 11:24:34 +0200

    inserting regex

--- client/src/components/Createacc/Createacc.js
+++ client/src/components/Createacc/Createacc.js
@@ -8,23 +8,39 @@ function Createacc() {
   const [password, setpassword] = useState("");
   const [confpass, setconfpass] = useState("");
   const [userlist, setuserlist] = useState([]);
+  const [passerr, setpasserr] = useState(false);
 
-  const addToList = async () => {
-    if (password === confpass) {
-      try {
-        const response = await Axios.post("http://localhost:3001/insertlogin", {
-          username: username,
-          password: password,
-        });
-        setuserlist([...userlist, response.data]);
-        console.log("account created!");
-      } catch (error) {
-        console.log("there is an error with addToList function");
-      }
+  // const validPass = new RegExp("^[a-zA-Z0-9._:$!%-]$");
+
+  const validPass = new RegExp(/^(?=.*?[a-z][A-Z][0-9]).+$/);
+
+  const addToList = () => {
+    if (!validPass.test(password)) {
+      console.log("password not right");
     } else {
-      console.log("pass and conf don't match");
+      console.log("cool password bro");
     }
   };
+
+  // const addToList = async () => {
+  //   if (password.length < 5) {
+  //     console.log("password too small");
+  //   }
+  //   if (password === confpass && password.length > 4) {
+  //     try {
+  //       const response = await Axios.post("http://localhost:3001/insertlogin", {
+  //         username: username,
+  //         password: password,
+  //       });
+  //       setuserlist([...userlist, response.data]);
+  //       console.log("account created!");
+  //     } catch (error) {
+  //       console.log("there is an error with addToList function");
+  //     }
+  //   } else {
+  //     console.log("pass and conf don't match");
+  //   }
+  // };
 
   return (
     <div className="login_div">
@@ -49,6 +65,7 @@ function Createacc() {
             value={confpass}
             onChange={(e) => setconfpass(e.target.value)}
           ></input>
+          <p>password must be at least 5 characters</p>
           <button type="button" onClick={addToList}>
             Create Account
           </button>
